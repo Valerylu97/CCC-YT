a83eff8061e8ba447238c7bd932e8d96a830e0fb
Valerylu97 <65468343+Valerylu97@users.noreply.github.com>
2021-01-30 17:38:09 -0500

    commit

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,7 @@
-const menu_icon = document.querySelector('header .menu-icon');
-const mobile_menu_icon = document.querySelector('header ul');
-const header = document.querySelector('header')
-const submenu = document.querySelector('.submenu')
+$(document).ready(main);
 
-/*habilitar y deabilitar las barritas del menu (arriba y abajo)*/
-menu_icon.addEventListener('click',() =>{
-    menu_icon.classList.toggle('active');
-    mobile_menu_icon.classList.toggle('active');
-});
+var contador = 1;
 
-/*scrolling menu*/
-document.addEventListener('scroll',()=>{
-    var scroll_position = window.scrollY;
-    if (scroll_position > 300){
-        header.style.backgroundColor = '#29323c';
-        submenu.style.backgroundColor = '#29323c';
-    } 
-    else{
-        header.style.backgroundColor = 'transparent';
-        submenu.style.backgroundColor = 'transparent';
-    }
-});
+function main(){
+    document.getElementsByClassName("menu-bar").click( document.getElementsByTagName("nav").toggle());
+}
