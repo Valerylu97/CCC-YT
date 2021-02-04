0979c6aeb4b91f5a8823b568e4577e777ee5c04f
Stalyn <stalyn151996@gmail.com>
2021-02-03 21:09:09 -0500

    send email with php

---
+++ php/enviar.php
@@ -0,0 +1,20 @@
+<?php
+//definir variables
+$mail = $_POST['mail'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Su e-mail es: " . $mail . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'stalyn151996@gmail.com';
+$asunto = 'Mensaje de PRECOMPC';
+
+mail($para, $asunto, utf8_decode($message), $header);
+echo "<script> alert('Por favor revisar su email')</script>";
+echo "<script> setTimeout(\"location.href='../index.html#contact'\", 1000)</script>";
+/*header("Location:../index.html#contact", 1000);*/
+?>
