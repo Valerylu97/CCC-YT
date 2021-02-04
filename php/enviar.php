0486a15861576e6391898d876431380094e5c737
Valerylu97 <65468343+Valerylu97@users.noreply.github.com>
2021-02-04 11:49:56 -0500

    cambios en enviar email

--- php/enviar.php
+++ php/enviar.php
@@ -7,15 +7,15 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Su e-mail es: " . $mail . " \r\n";
+$message = "Hola bienvenida/o a nuestro club. Para registrarte por favor llena el formulario adjunto acontinuación \r\n";
+$message .= "https://docs.google.com/forms/d/1A8DlSj3EB5q2L_ZjNKz-UuA7aJYc3yMkO-9EvgFKZwQ/prefill"."\r\n";
 $message .= "Enviado el: " . date('d/m/Y', time()) . " \r\n";
-$message .= "https://docs.google.com/forms/d/1A8DlSj3EB5q2L_ZjNKz-UuA7aJYc3yMkO-9EvgFKZwQ/prefill";
 
 $para = $mail;
 $asunto = 'Mensaje de PRECOMPC';
 
 mail($para, $asunto, utf8_decode($message), $header);
 echo "<script> alert('Por favor revisar su email')</script>";
-echo "<script> setTimeout(\"location.href='../index.html#contact'\", 1000)</script>";
+echo "<script> setTimeout(\"location.href='C:/Users/Legion/Desktop/CCC-YT/index.html'\", 1000)</script>";
 /*header("Location:../index.html#contact", 1000);*/
 ?>
