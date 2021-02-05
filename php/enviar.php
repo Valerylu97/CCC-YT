4da5d18d8afbaba686e41a86c0b17b5935b431a7
Stalyn <stalyn151996@gmail.com>
2021-02-05 01:14:53 -0500

    sms del email y pequeños detalles

--- php/enviar.php
+++ php/enviar.php
@@ -9,10 +9,12 @@ $header .= "Content-Type: text/plain";
 
 $message = "Hola bienvenida/o a nuestro club. Para registrarte por favor llena el formulario adjunto acontinuación \r\n";
 $message .= "https://docs.google.com/forms/d/1A8DlSj3EB5q2L_ZjNKz-UuA7aJYc3yMkO-9EvgFKZwQ/prefill"."\r\n";
+$message .= "De antemano muchas gracias por mostrar interés en nosotros y querer formar parte del mismo. ". " \r\n";
+$message .= "CCC-YT ". " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time()) . " \r\n";
 
 $para = $mail;
-$asunto = 'Mensaje de PRECOMPC';
+$asunto = 'Inscripción CCC-YT';
 
 mail($para, $asunto, utf8_decode($message), $header);
 echo "<script> alert('Por favor revisar su email')</script>";
